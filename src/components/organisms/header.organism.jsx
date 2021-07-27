d194e00f55c861b62b256aa805f394297cdefb4b
Muhammad Qasim <qasim9872@gmail.com>
2021-07-27 21:53:23 +0100

    initial header view

--- src/components/organisms/header.organism.jsx
+++ src/components/organisms/header.organism.jsx
@@ -1,4 +1,24 @@
 import React from 'react'
-import 'twin.macro'
+import tw from 'twin.macro'
 
-export const Header = () => <div tw="bg-red-100">Header</div>
+import { Logo } from '../atoms/logo'
+
+const Nav = tw.nav`bg-red-100 flex items-center justify-between px-4 py-2 left-0 top-0 w-full absolute z-10`
+const NavLogoHolder = tw.div`flex items-center justify-center`
+
+const NavLinksContainer = tw.ul`flex list-none`
+const NavLink = tw.li`cursor-pointer ml-2 mt-4 p-2 hover:text-blue-400`
+
+export const Header = () => (
+  <Nav>
+    <NavLogoHolder>
+      <Logo />
+      <h1> Stick'Me</h1>
+    </NavLogoHolder>
+    <NavLinksContainer>
+      <NavLink>Home</NavLink>
+      <NavLink>About</NavLink>
+      <NavLink>Blog</NavLink>
+    </NavLinksContainer>
+  </Nav>
+)
