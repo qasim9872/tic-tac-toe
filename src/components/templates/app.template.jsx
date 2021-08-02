6b7ff3e1a8f5bf1fa220a5db6ec1900c08f32621
Muhammad Qasim <qasim9872@gmail.com>
2021-08-02 12:57:12 +0100

    more cleanup

--- src/components/templates/app.template.jsx
+++ src/components/templates/app.template.jsx
@@ -1,21 +1,14 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import tw from 'twin.macro'
-
-import { Header } from '../organisms/header.organism'
-import { UseStickyContext } from '../../store/use-sticky.store'
 
 const AppContainer = tw.div`flex min-h-screen min-w-full bg-blue-100 justify-center`
 const AppTemplateWrapper = tw.div`container flex flex-col`
 const AppContent = tw.div`space-y-6`
 
 export const AppTemplate = ({ children }) => {
-  const { isSticky } = useContext(UseStickyContext)
-
   return (
     <AppContainer>
       <AppTemplateWrapper>
-        <Header sticky={isSticky} />
-
         <AppContent>{children}</AppContent>
       </AppTemplateWrapper>
     </AppContainer>
