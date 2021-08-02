0faec35e37ddef32d227b6df2d7cc6e46f23b7bd
Muhammad Qasim <qasim9872@gmail.com>
2021-08-02 16:03:57 +0100

    moving context to app js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 import 'twin.macro'
 
 import { Secrets } from './pages/secrets.page'
+import { SecretsContextProvider } from './services/secrets/secrets.context'
 
-const App = () => <Secrets />
+const App = () => (
+  <SecretsContextProvider>
+    <Secrets />
+  </SecretsContextProvider>
+)
 
 export default App
--- src/pages/secrets.page.jsx
+++ src/pages/secrets.page.jsx
@@ -3,14 +3,11 @@ import React from 'react'
 
 import { AppTemplate } from '../components/templates/app.template'
 import { Container } from '../components/atoms/utility/container.component'
-import { SecretsContextProvider } from '../services/secrets/secrets.context'
 
 export const Secrets = () => {
   return (
-    <SecretsContextProvider>
-      <AppTemplate>
-        <Container>This is the secrets page</Container>
-      </AppTemplate>
-    </SecretsContextProvider>
+    <AppTemplate>
+      <Container>This is the secrets page</Container>
+    </AppTemplate>
   )
 }
