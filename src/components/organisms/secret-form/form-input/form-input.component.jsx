01ffacaeeeac75388fca086b9e82b0cbadb95048
Muhammad Qasim <qasim9872@gmail.com>
2021-08-03 12:08:19 +0100

    updating form input component

--- src/components/organisms/secret-form/form-input/form-input.component.jsx
+++ src/components/organisms/secret-form/form-input/form-input.component.jsx
@@ -1,19 +1,15 @@
+import tw from 'twin.macro'
 import React from 'react'
-import './form-input.styles.scss'
+
+const FieldWrapper = tw.div`relative`
+const TextLabel = tw.label`leading-7 text-sm text-gray-600`
+const TextField = tw.input`w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`
 
 export const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" onChange={handleChange} {...otherProps} />
-      {label ? (
-        <label
-          className={`${
-            otherProps.value.length ? 'shrink' : ''
-          } form-input-label`}
-        >
-          {label}
-        </label>
-      ) : null}
-    </div>
+    <FieldWrapper>
+      {label ? <TextLabel>{label}</TextLabel> : null}
+      <TextField {...otherProps} />
+    </FieldWrapper>
   )
 }
