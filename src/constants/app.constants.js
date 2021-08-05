e8278a5742de80d432d530f36f793f757c238b13
Muhammad Qasim <qasim9872@gmail.com>
2021-08-05 22:04:57 +0100

    updating constants

--- src/constants/app.constants.js
+++ src/constants/app.constants.js
@@ -3,11 +3,11 @@ import { ProviderWithContextHoc } from '../utils/hoc/provider-with-context'
 const constants = {
   appName: 'Tic Tac Toe',
   author: 'Muhammad Qasim',
-  socials: {
-    twitter: '',
-    github: '',
-    linkedin: '',
-  },
+  socials: [
+    { icon: 'email', href: 'mailto:qasim9872@gmail.com' },
+    { icon: 'github', href: 'https://github.com/qasim9872' },
+    { icon: 'linkedin', href: 'https://www.linkedin.com/in/qasim9872/' },
+  ],
 }
 
 export const {
