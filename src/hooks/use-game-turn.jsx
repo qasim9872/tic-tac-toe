8b09e6e3a12bf3cb1c7b538e89f86fb22f6c7b39
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-08-06 15:05:39 +0100

    added game turn hook

---
+++ src/hooks/use-game-turn.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+const TOTAL_PLAYERS = 2
+
+export const useGameTurn = () => {
+  const [turn, setTurn] = useState()
+  const [player, setPlayer] = useState()
+
+  useEffect(() => {
+    // player 1 turn
+    setTurn(0)
+  }, [])
+
+  useEffect(() => setPlayer((turn % TOTAL_PLAYERS) + 1), [turn])
+
+  const next = () => setTurn(previousTurn => previousTurn + 1)
+
+  return { turn, next, player }
+}
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -6,21 +6,26 @@ import { Container } from '../components/atoms/utility/container.component'
 import { Banner } from '../components/molecules/banner/banner.component'
 import { Grid } from '../components/molecules/grid/grid.component'
 import { useGrid } from '../hooks/use-grid'
+import { useGameTurn } from '../hooks/use-game-turn'
 
 export const Home = () => {
   const { grid, mark, getMark } = useGrid()
+  const { next, player } = useGameTurn()
 
   const onClick = ({ x, y }) => {
     const current = getMark(x, y)
     console.log(`pressed (${x},${y}). `, { current })
 
-    mark(x, y, 'clicked')
+    mark(x, y, `Player-${player}`)
+    next()
   }
 
   return (
     <AppTemplate>
       <Container override={tw`flex-col`}>
         <Banner />
+
+        <div tw="text-lg pb-8">Player {player}</div>
 
         <Grid grid={grid} onClick={onClick} />
       </Container>
