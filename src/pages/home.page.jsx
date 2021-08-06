a81c2c63567a41952d800b461a59ee4ac42b86a2
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-08-06 17:12:52 +0100

    logic to ensure duplicate entries are not made

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import { AppTemplate } from '../components/templates/app.template'
 import { Container } from '../components/atoms/utility/container.component'
+import { Button } from '../components/atoms/form/button.component'
 import { Banner } from '../components/molecules/banner/banner.component'
 import { Grid } from '../components/molecules/grid/grid.component'
 import { useGrid } from '../hooks/use-grid'
@@ -18,9 +19,19 @@ export const Home = () => {
     2: <icons.gameCross tw="w-12 h-12" />,
   }
 
+  const preview = {
+    1: <icons.gameTick tw="w-8 h-8" />,
+    2: <icons.gameCross tw="w-8 h-8" />,
+  }
+
   const onClick = ({ x, y }) => {
     const current = getMark(x, y)
     console.log(`pressed (${x},${y}). `, { current })
+
+    if (current) {
+      alert(`invalid move. The box is already filled`)
+      return
+    }
 
     mark(x, y, map[player])
     next()
@@ -35,8 +46,17 @@ export const Home = () => {
 
         <Grid grid={grid} onClick={onClick} />
 
-        <div tw="flex justify-center items-center text-xl pb-8">
-          Player {player} {map[player]}
+        <div tw="flex flex-col justify-center items-center text-xl pt-8">
+          {[1, 2].map(p => (
+            <div
+              tw="flex items-center text-xl"
+              css={[player === p && tw`text-green-500`]}
+            >
+              Player {p} {'=>'} {preview[p]}
+            </div>
+          ))}
+
+          <Button onClick={restart}>Reset</Button>
         </div>
       </Container>
     </AppTemplate>
